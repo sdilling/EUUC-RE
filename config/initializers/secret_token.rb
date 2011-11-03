71f6264aba8e60e634ac54bad00dc9dbf0772613
Sarah Dilling <sarahd@gmail.com>
2011-11-02 20:49:43 -0700

    RoR start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EUUCRE::Application.config.secret_token = 'fe140e1c2d943cd58c328134f8ab5fb317e0490356f4b759d2ce78fbccc7025442d404222f245ddc8035dcbf7a4722e5f565336f9d21f4c24304b994b915c1b7'
