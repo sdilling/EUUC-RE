41fc40ca3cf982c8870bc477c755174e91144745
Sarah Dilling <sarahd@gmail.com>
2011-11-02 21:15:31 -0700

    change to mysql

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-EUUCRE::Application.config.secret_token = 'fe140e1c2d943cd58c328134f8ab5fb317e0490356f4b759d2ce78fbccc7025442d404222f245ddc8035dcbf7a4722e5f565336f9d21f4c24304b994b915c1b7'
+EUUCRE::Application.config.secret_token = '1f81ba6d82eb7fcd7afb61c9b6f758723bcdb41949a98bdf607c0c3f733c339150532cd201cd266f0d0497c7cde6e793ccfdbde0546f57f181ef4144ef3494aa'
