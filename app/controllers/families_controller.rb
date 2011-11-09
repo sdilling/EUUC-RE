f241ad908886bf97c8349c72472f24353119e059
Sarah Dilling <sarahd@wheezy.local>
2011-11-08 22:05:09 -0800

    person CRUD works now.

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,27 +1,27 @@
 class FamiliesController < ApplicationController
   http_basic_authenticate_with :name=> "sarahd", :password => "euucdb", :except => :index
-
+  before_filter :find_family, :only => [:show,
+                                        :edit,
+                                        :update,
+                                        :destroy]
   def index
     @families = Family.order("name ASC")
- 
     respond_to do |format|
       format.html  # index.html.erb
-      format.json  { render :json => @posts }
+      format.json  { render :json => @families }
     end
   end
 
   def new
     @family = Family.new
- 
     respond_to do |format|
       format.html  # new.html.erb
-      format.json  { render :json => @post }
+      format.json  { render :json => @family }
     end
   end
 
   def create
     @family = Family.new(params[:family])
- 
     respond_to do |format|
       if @family.save
         format.html  { redirect_to(@family,
@@ -37,21 +37,17 @@ class FamiliesController < ApplicationController
   end
 
   def show
-    @family = Family.find(params[:id])
- 
     respond_to do |format|
       format.html  # show.html.erb
-      format.json  { render :json => @post }
+      format.json  { render :json => @family }
     end
   end
 
   def edit
-    @family = Family.find(params[:id])
+
   end
 
   def update
-    @family = Family.find(params[:id])
- 
     respond_to do |format|
       if @family.update_attributes(params[:family])
         format.html  { redirect_to(@family,
@@ -66,13 +62,18 @@ class FamiliesController < ApplicationController
   end
 
   def destroy
-    @family = Family.find(params[:id])
     @family.destroy
- 
     respond_to do |format|
       format.html { redirect_to families_url }
       format.json { head :ok }
     end
   end
 
+  private
+    def find_family
+      @family = Family.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "That family isn't found."
+      redirect_to families_path
+    end
 end
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,17 +1,52 @@
 class PeopleController < ApplicationController
   http_basic_authenticate_with :name => "sarahd", :password => "euucdb", :only => :destroy
+  before_filter :find_family
+  before_filter :find_person, :only => [:destroy, :edit, :update]
+
+  def new
+    @person = @family.people.build
+  end
 
   def create
-    @family = Family.find(params[:family_id])
     @person = @family.people.create(params[:person])
     redirect_to family_path(@family)
   end
 
   def destroy
-    @family = Family.find(params[:family_id])
-    @person = @family.people.find(params[:id])
     @person.destroy
     redirect_to family_path(@family)
   end
+
+  def edit
+
+  end
+
+  def update
+    respond_to do |format|
+      if @person.update_attributes(params[:person])
+        format.html  { redirect_to(@family,
+                      :notice => 'Family was successfully updated.') }
+        format.json  { render :json => {}, :status => :ok }
+      else
+        format.html  { render :action => "edit" }
+        format.json  { render :json => @person.errors,
+                      :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def find_person
+      @person = @family.people.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "Didn't find the person"
+      redirect_to family_path(@family)
+    end
+
+  private
+    def find_family
+      @family = Family.find(params[:family_id])
+    end
+
 
 end
